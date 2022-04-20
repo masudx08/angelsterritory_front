fad6fe3f6fd0b8d19f235862ee1f285b63e3e63b
masudx08 <promasud225@gmail.com>
2022-04-20 14:16:51 +0600

    modal done

--- src/components/MySlider/Trade/MyModal.js
+++ src/components/MySlider/Trade/MyModal.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
-import './mymodal.css'
+import { Form, Modal } from "react-bootstrap";
+import "./mymodal.css";
 export default function MyModal(props) {
   return (
     <div>
@@ -12,17 +12,36 @@ export default function MyModal(props) {
         centered
       >
         <Modal.Body>
-          <h4>Setup Position</h4>
-          <p>Entry Coin</p>
-          <input type="text" value="USDT" />
-          <p>USDT Amount</p>
-          <input type="text" placeholder="Enter your amount" />
-          <div>
-            <p>% of your balance</p>
-            <p>Available balance: 0.919 USDT</p>
-            <p>Remaining balance: 0.919 USDT</p>
+          <div className="mymodalCont">
+            <h3 className="white-color text-center mb-3">Setup Position</h3>
+            <p className="small-text mb-1">Entry Coin</p>
+            <Form.Select aria-label="Default select example">
+              <option value="USDT">USDT</option>
+              <option value="BNB">BNB</option>
+            </Form.Select>
+            <div>
+              <p className="small-text mt-4 mb-1">USDT Amount</p>
+              <input type="text" placeholder="Enter your amount" />
+              <div className='amount_btn mt-2'>
+                <p>Min</p>
+                <p>0.25%</p>
+                <p>0.5%</p>
+                <p>1%</p>
+              </div>
+              <div className='amount_btn'>
+                <p>2.5%</p>
+                <p>5%</p>
+                <p>10%</p>
+                <p>25%</p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <p>% of your balance</p>
+              <p>Available balance: 0.919 USDT</p>
+              <p>Remaining balance: 0.919 USDT</p>
+            </div>
+            <h4 className="primary-color  text-end cursor">Lock & Enter</h4>
           </div>
-          <h4>Lock & Enter</h4>
         </Modal.Body>
       </Modal>
     </div>
--- src/pages/Trade/Trade.js
+++ src/pages/Trade/Trade.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { MyContext } from "../../MainContext";
+
 import {
   getCartsFetch,
   upPoolFetch,
